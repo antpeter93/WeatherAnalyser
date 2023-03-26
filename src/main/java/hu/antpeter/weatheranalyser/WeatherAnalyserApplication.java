ed29832e428c76f75704cdc14c407c3659c1ac58
Peti <antpeter93@gmail.com>
2023-03-26 09:32:50 +0200

    init

---
+++ src/main/java/hu/antpeter/weatheranalyser/WeatherAnalyserApplication.java
@@ -0,0 +1,13 @@
+package hu.antpeter.weatheranalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/antpeter/weatheranalyser/WeatherAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.antpeter.weatheranalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
